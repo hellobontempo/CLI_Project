93a386b8f5e57efdd8c2d18cb50587ad924255d5
claire <clairebontempo@gmail.com>
2020-11-10 13:57:16 -0800

    display

--- lib/recipe_finder/cli.rb
+++ lib/recipe_finder/cli.rb
@@ -1,25 +1,26 @@
 
 class Cli
-    @@input = []
+    #@@input = []
 
     def start
         puts "             Hey good lookin' - whatcha got cookin'?"   
         puts "  But really, what's in your fridge? I'll help you find a recipe!"         
         puts "              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"       
         puts "Type 3 ingredients you would like to use. Hit 'enter' after each ingredient."
-        self.user_ingredients
-        api = Api.new(Cli.input[0], Cli.input[1], Cli.input[2])
+        self.user_input
+        api = Api.new(Cli.sort[0], Cli.sort[1], Cli.sort[2])
         binding.pry
     end
 
-    def user_ingredients #how to handle user data entered in differently, make a method 
+    def user_input #how to handle user data entered in differently, make a method 
+        @@input = []
         ing_1 = gets.chomp 
         ing_2 = gets.chomp
         ing_3 = gets.chomp
         @@input << ing_1 << ing_2 << ing_3
     end 
 
-    def self.input
+    def self.sort
         @@input.sort
     end
 
